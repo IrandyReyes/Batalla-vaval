5fc447d16add05d564e9f4c6241b17610efa115d
IrandyReyes <83356104+IrandyReyes@users.noreply.github.com>
2021-05-02 15:26:28 -0500

    Add files via upload

---
+++ Batalla naval.php
@@ -0,0 +1,52 @@
+<?php
+    echo "<head>
+    <meta charset=utf-8>
+    <title>Bata Naval</title>
+    </head>
+    <body>
+        <form action=./Batalla naval.php method=get>
+            <fieldset>
+            <legend><h1>Batalla Naval</h1></legend>
+            <h2><strong>Vidas</strong></h3>
+            <p>Historial de disparos:</p>
+            <label for=Nguests class=col-md-2 col-form-label>Posición X(Letra):</label>
+            <input type=text name=X required>
+            <label for=Nguests class=col-md-2 col-form-label>Posición Y(Número):</label>
+            <input type=number name=Y required>
+            <input type=submit value=Disparar>
+            </fieldset>
+        </form>
+    </body>";
+
+    /*<head>
+    <meta charset=utf-8>
+    <title>Bata Naval</title>
+    </head>
+    <body>
+        <form action=./Batalla naval.php method=get>
+            <fieldset>
+            <label for=Nguests class=col-md-2 col-form-label></label>
+            <input class= form-check-input type=radio name=8 id=NGuest1 value=A checked>
+            <label class=form-check-label for=NGuest1>Feliz</label>
+            <input class=form-check-input type=radio name=8 id=NGuest2 value=B checked>
+            <label class=form-check-label for=NGuest2>Con mucha energía</label>
+            <input class=form-check-input type=radio name=8 id=NGuest3 value=C checked>
+            <label class=form-check-label for=NGuest3>Desanimado</label>
+            <input class=form-check-input type=radio name=8 id=NGuest4 value=D checked>
+            <label class=form-check-label for=NGuest4>Nostálgico</label>
+            </fieldset>
+        </form>
+    </body>;
+    <meta charset=utf-8>
+    <title>Bata Naval</title>
+    <h3>Vidas</h3> 
+    <label for=Nguests class=col-md-2 col-form-label></label>
+    <input class= form-check-input type=radio name=8 id=NGuest1 value=A checked>
+    <label class=form-check-label for=NGuest1>Feliz</label>
+    <input class=form-check-input type=radio name=8 id=NGuest2 value=B checked>
+    <label class=form-check-label for=NGuest2>Con mucha energía</label>
+    <input class=form-check-input type=radio name=8 id=NGuest3 value=C checked>
+    <label class=form-check-label for=NGuest3>Desanimado</label>
+    <input class=form-check-input type=radio name=8 id=NGuest4 value=D checked>
+    <label class=form-check-label for=NGuest4>Nostálgico</label>"*/
+?>
