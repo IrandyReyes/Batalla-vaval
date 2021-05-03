08bc1c402224a351a36b6edb88db57d6cbfbb0db
IrandyReyes <83356104+IrandyReyes@users.noreply.github.com>
2021-05-03 18:23:35 -0500

    Add files via upload

--- Batalla naval.php
+++ Batalla naval.php
@@ -7,8 +7,7 @@
             <title>Bata Naval</title>
             </head>";
         
-        echo "<form action=./Batalla naval.php method=get>
-            <legend><h1>Batalla Naval</h1></legend>
+        echo "<legend><h1>Batalla Naval</h1></legend>
             <h2><strong>Vidas:</strong></h3>";
             for($vi=9; $vi>0; $vi--)
             {
@@ -17,12 +16,26 @@
         
         echo "<p>Historial de disparos:</p>";
             
-        echo "<label for=Nguests class=col-md-2 col-form-label>Posición X(Letra):</label>
-            <input type=text name=X required>
+        echo "<form action=./Batalla naval.php method=>
+            <label for=Nguests class=col-md-2 col-form-label>Posición X(Letra):</label>
+            <select name= X>
+                    <option value=A>A</option>
+                    <option value=B>B</option>
+                    <option value=C>C</option>
+                    <option value=D>D</option>
+                    <option value=E>E</option>
+                    <option value=F>F</option>
+                    <option value=G>G</option>
+                    <option value=H>H</option>
+                    <option value=I>I</option>
+                    <option value=J>J</option>
+                </select>
             <label for=Nguests class=col-md-2 col-form-label>Posición Y(Número):</label>
-            <input type=number name=Y required>
-            <input type=submit value=Disparar>
-            </form>";
+            <input type=number name=Y min= 1 max= 10 required>
+            <input type=submit value=Disparar></form>";
+        
+        $X=(isset($_POST["X"]) && $_POST["X"]!= "") ?$_POST["X"]: "Inválido";
+        $Y=(isset($_POST["Y"]) && $_POST["Y"]!= "") ?$_POST["Y"]: "Inválido";
         $vidas-=1;
     }
 ?>
